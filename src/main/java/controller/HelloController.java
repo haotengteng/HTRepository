70be5c8c51e67ea11cb88a08147cc05c9c4c307e
haotengteng <1798440444@qq.com>
2015-12-23 08:09:32 +0800

    create

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
     @ResponseBody
     @RequestMapping(value = "/hello")
-    public String sayHello(int i) throws Exception {
-        if (i==1){
-            throw new Exception();
-        }
+    public String sayHello(String userName,String password) throws Exception {
+
         return "hello world";
     }
 
